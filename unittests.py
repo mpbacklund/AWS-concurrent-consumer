3b52f53bbee0ce3b7cca443f029b1d08059dc24a
Matt Backlund <mpbacklund99@gmail.com>
2024-12-13 16:16:41 -0700

    final commit

--- unittests.py
+++ unittests.py
@@ -1,10 +1,1 @@
-import unittest
-from click.testing import CliRunner
-from parse import parse, get_config  # Replace with your actual module name
 
-class TestClickCommands(unittest.TestCase):
-    
-    def test_
-
-if __name__ == '__main__':
-    unittest.main()
